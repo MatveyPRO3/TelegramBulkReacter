2145383ae63c5f6dd1a4086fd0bf30fb509a06fe
MatveyPRO32 <matveyrotte@gmail.com>
2024-03-09 15:59:48 +0300

    initial

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, errors
 import os
 from api_info import *
 
 if not os.path.exists("pyrogram_session"):
     os.makedirs("pyrogram_session")
 
+CHAT_ID = int(input("Chat id: "))
+REACTION = input("Valid reaction (eg 🗿): ")
+limit = int(input("Number of messages (including your own): "))
 
-app = Client(
+with Client(
     "my_session",
     workdir="pyrogram_session/",
     api_hash=api_hash,
     api_id=api_id,
-)
+) as app:
 
-CHAT_ID = "Your chat id"
-
-
-@app.on_message(filters.chat(CHAT_ID))
-def handle_group_messages(client, message):
-    if not message.from_user.is_self:
-        app.send_reaction(message.chat.id, message.id, "🗿")
-
-
-app.run()
+    messages = app.get_chat_history(CHAT_ID, limit=limit)
+    for m in messages:
+        if m.from_user.is_self:
+            continue
+        try:
+            app.send_reaction(CHAT_ID, m.id, REACTION)
+        except (
+            errors.exceptions.bad_request_400.MessageIdInvalid,
+            errors.exceptions.bad_request_400.MessageNotModified,
+        ):
+            pass  # message does not exist or reaction is already sent and matches
